78f497d42b4f06c0845ca4d8e12e81c86d4945aa
Hazel Jane Lihotan <anclamcfrancis037@gmail.com>
2025-09-05 10:33:34 +0800

    javascript basic

---
+++ hello.js
@@ -0,0 +1,20 @@
+console.log ("Hello Universe!");
+
+let student = "Hazel Jane";
+let grade = 1.0;
+let isPassed = true;
+let address;
+let partner = null;
+
+console.log ("Student Name :" +student+ "\nFinal Grade :" +grade+ "\nPassed?:" +isPassed+ "\naddress :" +address+ "\nPartner :" +partner+ "\n" );
+
+let num1= 10;
+let num2= 5;
+
+let sum= num1+num2;
+let diff= num1-num2;
+let prod= num1*num2;
+let quo= num1/num2;
+let mod= num1%num2;
+
+console.log ("Sum: " +sum+ "\nDifferent: " +diff+ "\nProduct: " +prod+ "\nQuotient: " +quo+ "\nModule: " +mod+ "\n" );
