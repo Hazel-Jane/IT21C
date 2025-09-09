59e730d8026719abceb0a35eb0f4099280c3566d
Hazel Jane Lihotan <marcdavetangaro0@gmail.com>
2025-09-09 10:13:58 +0800

    Added operators for daily routine

--- LihotanLife.js
+++ LihotanLife.js
@@ -1,8 +1,18 @@
 console.log ("Hello Universe");
 
-let student = "Hazel Jane Lihotan";
-let course = "Bachelor of Science in Information Technology";
-let year = "2nd year";
+// let student = "Hazel Jane Lihotan";
+// let course = "Bachelor of Science in Information Technology";
+// let year = "2nd year";
 
 
-console.log ("Name :" +student+ "\nCourse :" +course+ "\nYear:" +year+ "\n" );
+// console.log ("Name :" +student+ "\nCourse :" +course+ "\nYear:" +year+ "\n" );
+
+let hoursleep = 5;
+let hourstudy = 3;
+
+let sum = hoursleep + hourstudy;
+let diff = hoursleep - hourstudy;
+
+console.log ("Total hours of sleep: " +sum+ "\nTotal hours of study: " +diff+ "\n" );
+
+
