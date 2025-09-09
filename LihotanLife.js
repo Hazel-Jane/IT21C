af0c18fc4fca8ea474eaa8b11d49f705607d7d98
Hazel Jane Lihotan <marcdavetangaro0@gmail.com>
2025-09-09 09:47:52 +0800

    Added student Variable

---
+++ LihotanLife.js
@@ -0,0 +1,8 @@
+console.log ("Hello Universe");
+
+let student = "Hazel Jane Lihotan";
+let course = "Bachelor of Science in Information Technology";
+let year = "2nd year";
+
+
+console.log ("Name :" +student+ "\nCourse :" +course+ "\nYear:" +year+ "\n" );
--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-console.log ("Hello Universe!");
+/*console.log ("Hello Universe");
 
 let student = "Hazel Jane";
 let grade = 1.0;
@@ -18,3 +18,36 @@ let quo= num1/num2;
 let mod= num1%num2;
 
 console.log ("Sum: " +sum+ "\nDifferent: " +diff+ "\nProduct: " +prod+ "\nQuotient: " +quo+ "\nModule: " +mod+ "\n" );
+
+// conditional
+
+
+let grade = 70;
+
+if(grade > 75){
+    console.log("students passed");
+    }else{
+        console.log("students failed");
+    }
+//loop
+
+for(let i = 0; i<=5; i++){
+    console.log("count: " + i);
+
+}
+
+// while loop
+
+let i = 0;
+
+while (i<5){
+    console.log("count: " +i);
+    i++;
+}
+    */
+   // function
+
+   function greet(name){
+    console.log("hello, " + name);
+   }
+   greet("hazel");
